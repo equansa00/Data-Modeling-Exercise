0c6a6f657b956aa04e8ce02da4581740f018be25
equansa00 <Your GitHub Email>
2023-09-12 17:14:01 -0400

    added visual images of the data models Medical_center, craigslist and soccer_league

---
+++ Craigslist.sql
@@ -0,0 +1,75 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "Regions" (
+    "region_id" SERIAL   NOT NULL,
+    "region_name" VARCHAR(255)   NOT NULL,
+    CONSTRAINT "pk_Regions" PRIMARY KEY (
+        "region_id"
+     ),
+    CONSTRAINT "uc_Regions_region_name" UNIQUE (
+        "region_name"
+    )
+);
+
+CREATE TABLE "Users" (
+    "user_id" SERIAL   NOT NULL,
+    "username" VARCHAR(255)   NOT NULL,
+    "password" VARCHAR(255)   NOT NULL,
+    "preferred_region_id" INT   NOT NULL,
+    CONSTRAINT "pk_Users" PRIMARY KEY (
+        "user_id"
+     ),
+    CONSTRAINT "uc_Users_username" UNIQUE (
+        "username"
+    )
+);
+
+CREATE TABLE "Posts" (
+    "post_id" SERIAL   NOT NULL,
+    "title" VARCHAR(255)   NOT NULL,
+    "text" TEXT   NOT NULL,
+    "user_id" INT   NOT NULL,
+    "location" VARCHAR(255)   NOT NULL,
+    "region_id" INT   NOT NULL,
+    "category_id" INT   NOT NULL,
+    CONSTRAINT "pk_Posts" PRIMARY KEY (
+        "post_id"
+     )
+);
+
+CREATE TABLE "Categories" (
+    "category_id" SERIAL   NOT NULL,
+    "category_name" VARCHAR(255)   NOT NULL,
+    CONSTRAINT "pk_Categories" PRIMARY KEY (
+        "category_id"
+     ),
+    CONSTRAINT "uc_Categories_category_name" UNIQUE (
+        "category_name"
+    )
+);
+
+CREATE TABLE "PostCategories" (
+    "post_id" INT   NOT NULL,
+    "category_id" INT   NOT NULL
+);
+
+ALTER TABLE "Users" ADD CONSTRAINT "fk_Users_preferred_region_id" FOREIGN KEY("preferred_region_id")
+REFERENCES "Regions" ("region_id");
+
+ALTER TABLE "Posts" ADD CONSTRAINT "fk_Posts_user_id" FOREIGN KEY("user_id")
+REFERENCES "Users" ("user_id");
+
+ALTER TABLE "Posts" ADD CONSTRAINT "fk_Posts_region_id" FOREIGN KEY("region_id")
+REFERENCES "Regions" ("region_id");
+
+ALTER TABLE "Posts" ADD CONSTRAINT "fk_Posts_category_id" FOREIGN KEY("category_id")
+REFERENCES "Categories" ("category_id");
+
+ALTER TABLE "PostCategories" ADD CONSTRAINT "fk_PostCategories_post_id" FOREIGN KEY("post_id")
+REFERENCES "Posts" ("post_id");
+
+ALTER TABLE "PostCategories" ADD CONSTRAINT "fk_PostCategories_category_id" FOREIGN KEY("category_id")
+REFERENCES "Categories" ("category_id");
+
---
+++ Medical_Center.sql
@@ -0,0 +1,67 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- Modify this code to update the DB schema diagram.
+-- To reset the sample schema, replace everything with
+-- two dots ('..' - without quotes).
+
+CREATE TABLE "Doctors" (
+    "doctor_id" SERIAL   NOT NULL,
+    "first_name" VARCHAR(100)   NOT NULL,
+    "last_name" VARCHAR(100)   NOT NULL,
+    "specialty" VARCHAR(150)   NOT NULL,
+    CONSTRAINT "pk_Doctors" PRIMARY KEY (
+        "doctor_id"
+     )
+);
+
+CREATE TABLE "Patients" (
+    "patient_id" SERIAL   NOT NULL,
+    "first_name" VARCHAR(100)   NOT NULL,
+    "last_name" VARCHAR(100)   NOT NULL,
+    "date_of_birth" DATE   NOT NULL,
+    CONSTRAINT "pk_Patients" PRIMARY KEY (
+        "patient_id"
+     )
+);
+
+CREATE TABLE "Diseases" (
+    "disease_id" SERIAL   NOT NULL,
+    "disease_name" VARCHAR(255)   NOT NULL,
+    "description" TEXT   NOT NULL,
+    CONSTRAINT "pk_Diseases" PRIMARY KEY (
+        "disease_id"
+     )
+);
+
+CREATE TABLE "Visits" (
+    "visit_id" SERIAL   NOT NULL,
+    "patient_id" INT   NOT NULL,
+    "doctor_id" INT   NOT NULL,
+    "date_of_visit" DATE   NOT NULL,
+    CONSTRAINT "pk_Visits" PRIMARY KEY (
+        "visit_id"
+     )
+);
+
+CREATE TABLE "Diagnoses" (
+    "diagnosis_id" SERIAL   NOT NULL,
+    "visit_id" INT   NOT NULL,
+    "disease_id" INT   NOT NULL,
+    CONSTRAINT "pk_Diagnoses" PRIMARY KEY (
+        "diagnosis_id"
+     )
+);
+
+ALTER TABLE "Visits" ADD CONSTRAINT "fk_Visits_patient_id" FOREIGN KEY("patient_id")
+REFERENCES "Patients" ("patient_id");
+
+ALTER TABLE "Visits" ADD CONSTRAINT "fk_Visits_doctor_id" FOREIGN KEY("doctor_id")
+REFERENCES "Doctors" ("doctor_id");
+
+ALTER TABLE "Diagnoses" ADD CONSTRAINT "fk_Diagnoses_visit_id" FOREIGN KEY("visit_id")
+REFERENCES "Visits" ("visit_id");
+
+ALTER TABLE "Diagnoses" ADD CONSTRAINT "fk_Diagnoses_disease_id" FOREIGN KEY("disease_id")
+REFERENCES "Diseases" ("disease_id");
+
